7b6b9db591d776e35a218174095ad103ca52809c
Andrew Schaaf <andrew@andrewschaaf.com>
2010-11-13 17:22:13 -0500

    commented out ExceptionsMiddleware

--- middleware.py
+++ middleware.py
@@ -1,20 +1,20 @@
 
 from dev_deployment import settings
 
-from util.exceptions import exceptionRepr
-from dev_deployment.debug import jab
+#from util.exceptions import exceptionRepr
+#from dev_deployment.debug import jab
 
 
 class ExceptionsMiddleware:
     
     def process_exception(self, r, exception):
         
-        if settings.DEBUG and settings.EXCEPTION_JABBER is not None:
-            jab(
-                    exceptionRepr(),
-                    settings.EXCEPTION_JABBER['to_account'],
-                    settings.EXCEPTION_JABBER['from_account'],
-                    settings.EXCEPTION_JABBER['from_password'])
+        #if settings.DEBUG and settings.EXCEPTION_JABBER is not None:
+        #    jab(
+        #            exceptionRepr(),
+        #            settings.EXCEPTION_JABBER['to_account'],
+        #            settings.EXCEPTION_JABBER['from_account'],
+        #            settings.EXCEPTION_JABBER['from_password'])
         
         # return HttpResponse ===> Django returns it to browser
         # else                ===> default exception handling
